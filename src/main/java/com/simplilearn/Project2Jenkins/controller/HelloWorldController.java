dd10a036381469c93291d237ebde4a9c39f3dbe6
Natacha Lafontaine <102371985+natacha-lafontaine@users.noreply.github.com>
2022-07-03 19:09:04 -0400

    more fixes

--- src/main/java/com/simplilearn/Project2Jenkins/controller/HelloWorldController.java
+++ src/main/java/com/simplilearn/Project2Jenkins/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.simplilearn.Project2Jenkins.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("/bookzy")
-    public String HelloWorld(){
+    @RequestMapping("/bookzy")
+    public String index(){
         return "Hello Welcome to Bookzy!";
     }    
 }
